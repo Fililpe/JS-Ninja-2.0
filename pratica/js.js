5046e274af7e43d88c39bd9da3ae94822e9cbec9
Filipe Sousa <lfilipesousa17@gmail.com>
2023-07-19 20:08:29 -0300

    ;

---
+++ pratica/js.js
@@ -0,0 +1,29 @@
+let passwordLength = '16'
+const inputEl = document.querySelector("#password")
+
+function generatePassword() {
+    const chars = "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789.+-[]*_@#$?"
+
+    let password = ""
+
+        for (let i = 0; i < passwordLength; i++) {
+            const randomNumber = Math.floor(Math.random() * chars.length)
+            password += chars.substring(randomNumber, randomNumber + 1)
+        }
+
+    
+     inputEl.value = password    
+    }
+
+    const copyButtonEl = document.querySelector("#copy")
+    copyButtonEl.addEventListener("click", function () {
+        navigator.clipboard.writeText(inputEl.value)
+    })
+
+    const passwordLengthEl = document.querySelector("#password-length")
+    passwordLengthEl.addEventListener("input", function(){
+        passwordLength = passwordLengthEl.value 
+        generatePassword()
+    })
+
+generatePassword()
